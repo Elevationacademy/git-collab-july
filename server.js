12f3a128313105adb4225610e02b1a4a2c1fbf6c
Corinne <corinnegottlieb@gmail.com>
2019-07-15 14:59:55 +0300

    console log

--- server.js
+++ server.js
@@ -8,23 +8,3 @@ const api = require('./server/routes/api')
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/weatherApp', { useNewUrlParser: true })
 
 const app = express()
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-app.use(express.static(path.join(__dirname, 'dist')))
-app.use(express.static(path.join(__dirname, 'node_modules')))
-
-console.log("SERVER IS HERE")
-
-
-
-
-app.use('/', api)
-
-const port = 8888
-
-app.listen(process.env.PORT || port, function() {
-    console.log(`Runnin runnin and runnin runnin on port ${port}`)
-})
-
