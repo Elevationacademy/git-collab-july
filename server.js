7137ad83b84e440cef5e1294f7b419b9b835f9ae
Corinne <corinnegottlieb@gmail.com>
2019-07-15 15:01:40 +0300

    console log

--- server.js
+++ server.js
@@ -1,10 +1,1 @@
-const express = require('express')
-const path = require('path')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const api = require('./server/routes/api')
-
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/weatherApp', { useNewUrlParser: true })
-
-const app = express()
+const server = "so exciting"
